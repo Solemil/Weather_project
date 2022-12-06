b6b3054b1f2ad4628140c1f8d9033400b866b5d6
Émile HB <euroemile@gmail.com>
2022-12-06 13:32:40 +0100

    Add the Weather app first version, with hard coded data

--- Weather_project/src/app/app.component.ts
+++ Weather_project/src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Weather_project';
+
+  cities = [
+    {
+      name: 'Budapest',
+      country: 'Hungary',
+      temp: '16',
+      weather: 'cloudy',
+    },
+    {
+      name: 'Paris',
+      country: 'France',
+      temp: '20',
+      weather: 'sunny',
+    },
+    {
+      name: 'London',
+      country: 'United Kingdom',
+      temp: '14',
+      weather: 'rainy',
+    },
+  ];
 }
